7c0f987b13718356e5e61248d052a2f75c009c61
PranavDuggal <pranavduggal1999@yahoo.com>
2021-08-10 21:16:38 +0530

    initial commit

---
+++ src/test/java/FB/TestNGDemo.java
@@ -0,0 +1,23 @@
+package FB;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestNGDemo {
+	@Test
+	public void m1() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Title-"+ driver.getTitle());
+		  
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+		
+	}
+}
